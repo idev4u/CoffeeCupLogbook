beae41266287f1e3924281ee965989341da63bdc
Norman Sutorius <us.idev.student@googlemail.com>
2025-11-29 12:08:18 +0100

    Add the counter logic

--- app/controllers/logbooks_controller.rb
+++ app/controllers/logbooks_controller.rb
@@ -1,5 +1,35 @@
 class LogbooksController < ApplicationController
+  
   def index
     @logbooks = Logbook.all
+    @logbook_sum = 0
+    @logbooks.each do | logbook |
+      @logbook_sum=@logbook_sum+logbook.amount
+    end
+
   end
+
+  def show
+    @logbook = Logbook.find(params[:id])
+  end
+
+  def new
+    @logbook = Logbook.new
+  end
+
+  def create
+    @logbook = Logbook.new(loogbook_params)
+    puts "XXX: #{@logbook}"
+    if @logbook.save
+      redirect_to @logbook
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def loogbook_params
+      params.expect(logbook: [ :cup_type, :amount ])
+    end
+
 end
