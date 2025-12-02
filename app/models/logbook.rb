7733cacc952805141ffdf3434289b981d4df1e79
Norman Sutorius <us.idev.student@googlemail.com>
2025-12-02 09:25:15 +0100

    Add input validation

--- app/models/logbook.rb
+++ app/models/logbook.rb
@@ -1,2 +1,9 @@
 class Logbook < ApplicationRecord
+
+    validates :cup_type, presence: true, length: { maximum: 100 }
+    validates :amount, presence: true, numericality: {
+              only_integer: true,
+              greater_than: 0,
+              less_than_or_equal_to: 10
+            }
 end
