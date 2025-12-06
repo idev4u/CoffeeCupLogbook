28245c4970198d1fec27f6c373615efd1e6f0c38
Norman Sutorius <us.idev.student@googlemail.com>
2025-12-06 13:55:18 +0100

    introduce the coffee package counter

--- app/controllers/logbooks_controller.rb
+++ app/controllers/logbooks_controller.rb
@@ -3,10 +3,19 @@ class LogbooksController < ApplicationController
   def index
     # @logbooks = Logbook.all
     @logbooks = Logbook.where(cup_type: 'Coffee')
+    @package_logbook = Logbook.where(cup_type: 'Plauen Gold Package')
     @logbook_sum = 0
     @logbooks.each do | logbook |
       @logbook_sum=@logbook_sum+logbook.amount
     end
+    
+    puts "XXX: #{@package_logbook}"
+    @package_logbook_sum = 0
+
+    @package_logbook.each do | package |
+      @package_logbook_sum=@package_logbook_sum+package.amount
+    end
+    puts "XXX: #{@package_logbook_sum}"
 
   end
 
