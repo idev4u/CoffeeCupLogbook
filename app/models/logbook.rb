8746cc236815e8ae799b28c911f235f45eee5dfc
Norman Sutorius <us.idev.student@googlemail.com>
2025-12-08 20:03:44 +0100

    persist the correct size_gram of the package

--- app/models/logbook.rb
+++ app/models/logbook.rb
@@ -1,4 +1,5 @@
 class Logbook < ApplicationRecord
+    before_validation :set_package_size_from_type
 
     validates :cup_type, presence: true, length: { maximum: 100 }
     validates :amount, presence: true, numericality: {
@@ -13,6 +14,17 @@ class Logbook < ApplicationRecord
     def package_size_required_for_package
       if cup_type == "Package" && package_size_grams.blank?
         errors.add(:package_size_grams, "must be set for packages")
+      end
+    end
+
+    def set_package_size_from_type
+      case cup_type
+      when "Probe Package"
+        self.package_size_grams = 250
+      when "Plauen Gold Package"
+        self.package_size_grams = 500
+      else
+        self.package_size_grams = nil
       end
     end
 end
