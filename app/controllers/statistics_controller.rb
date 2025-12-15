4843a06f2f20117152093602a0090c4db11f3e40
Norman Sutorius <us.idev.student@googlemail.com>
2025-12-15 07:29:48 +0100

    statistics page show the correct amount of coffee cups

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,16 +3,18 @@ class StatisticsController < ApplicationController
     scope = Logbook.all
 
     today      = Time.zone.today
-    @today     = scope.where(created_at: today.all_day).sum(:amount)
-    @this_week = scope.where(created_at: today.all_week).sum(:amount)
-    @this_month = scope.where(created_at: today.all_month).sum(:amount)
+    @today     = scope.where(cup_type: "Coffee", created_at: today.all_day).sum(:amount)
+    @this_week = scope.where(cup_type: "Coffee", created_at: today.all_week).sum(:amount)
+    @this_month = scope.where(cup_type: "Coffee", created_at: today.all_month).sum(:amount)
     @total     = scope.where(cup_type: "Coffee").sum(:amount)
 
     @cups_per_day = scope
+      .where(cup_type: "Coffee")
       .group_by_day(:created_at, last: 14)
       .sum(:amount)
 
     @cups_per_month = scope
+      .where(cup_type: "Coffee")
       .group_by_month(:created_at, last: 6)
       .sum(:amount)
 
